480f2ea7168a55bdd4853eb6d0c2f289fd7b2142
JustinLGates <papagates1@gmail.com>
2020-09-16 00:22:32 -0600

    worked on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,47 @@
 <template>
-  <div class="col-12">
+  <div class="col-12 p-0">
     <div class="row">
       <div class="col-12">
-        <h5>Home</h5>
+        <h1 class="text-center text-primary py-5">More than web design</h1>
       </div>
-     <div class="col-12">
-       <h1>Cementing your digital presence</h1>
-     </div>
-     
     </div>
-    
-     
+    <div class="row">
+      <div class="p-0 col-12 col-lg-6 py-4 bg-dark text-light">
+        <h3 class="text-primary p-2 m-0">Seo</h3>
+        <p
+          class="px-3 m-0"
+        >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id ad dolore at voluptatum ipsum animi incidunt assumenda recusandae</p>
+
+        <h3 class="text-primary p-2 m-0">Marketing</h3>
+        <p
+          class="px-3 m-0"
+        >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id ad dolore at voluptatum ipsum animi incidunt assumenda recusandae</p>
+        <h3 class="text-primary p-2 m-0">social media</h3>
+        <p
+          class="px-3 m-0"
+        >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id ad dolore at voluptatum ipsum animi incidunt assumenda recusandae</p>
+      </div>
+      <div class="p-0 col-12 col-lg-6 bg-dark text-light">
+        <h3 class="text-primary p-2 py-lg-4 m-0">Marketing plan talored to your bussiness</h3>
+        <p
+          class="px-3 m-0"
+        >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id ad dolore at voluptatum ipsum animi incidunt assumenda recusandae</p>
+        <div class="pos-rel pt-4 m-auto">
+          <div class="d-flex justify-content-center">
+            <img class="img-fluid img-1 p-2 p-lg-4" src="https://placehold.it/500x400" alt />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 col-lg-6 spacer-top-1">
+        <img class="img-fluid" src="https://placehold.it/500x400" alt />
+      </div>
+      <div class="spacer-top-2 col-12 col-lg-6">
+        <h3>Cementing your digital precence</h3>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo hic recusandae esse maiores qui cum quae distinctio reprehenderit. Doloremque fuga ipsa quas sed enim inventore aliquid nesciunt quae quaerat ipsum?</p>
+      </div>
     </div>
   </div>
 </template>
@@ -20,27 +51,31 @@ import Services from "../components/ServicesComp";
 
 export default {
   name: "Home",
-  components: {
-
-  },
+  components: {},
 };
 </script>
 
 <style>
-.photo-caption {
-  position: absolute;
-  bottom: 50px;
-  background-image: linear-gradient(
-    to bottom right,
-    rgba(238, 146, 33, 0.842),
-    rgba(255, 136, 0, 0.219)
-  );
-  text-align: left;
-  color: rgb(255, 255, 255);
-  width: 90%;
-  box-shadow: 2px 2px 2px rgba(255, 140, 0, 0.493);
+.pos-rel {
+  position: relative;
 }
-.hero {
-  max-height: 30vh;
+.img-1 {
+  position: absolute;
+  top: 100px;
+}
+
+.spacer-top-1 {
+  padding-top: 50px;
+}
+.spacer-top-2 {
+  padding-top: 300px;
+}
+@media only screen and (max-width: 942px) {
+  .spacer-top-1 {
+    padding: 0.5rem;
+  }
+  .img-1 {
+    position: relative;
+  }
 }
 </style>
